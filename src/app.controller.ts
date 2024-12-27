0fbee7f36e45099d359e2b4d8ad14b87f898e0da
Abhay kashyap <abhaykashyap89@gmail.com>
2024-12-27 09:52:58 +0530

    Logger service created and logger service integrated in AppController

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,17 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { LoggerService } from './logger/logger.service';
 
 @Controller('base')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly loggerService: LoggerService
+  ) {}
 
   @Get()
   getHello(): string {
+    this.loggerService.log('getHello called');
     return this.appService.getHello();
   }
 }
---
+++ src/logger/logger.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@nestjs/common';
+import * as winston from 'winston';
+
+@Injectable()
+export class LoggerService {
+  private logger: winston.Logger;
+
+  constructor() {
+    this.logger = winston.createLogger({
+      level: 'info',
+      format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.printf(({ timestamp, level, message }) => {
+          return `${timestamp} [${level}]: ${message}`;
+        })
+      ),
+      transports: [
+        new winston.transports.Console(),
+        // Add other transports like File if needed
+      ],
+    });
+  }
+
+  log(message: string) {
+    this.logger.info(message);
+  }
+
+  error(message: string, trace: string) {
+    this.logger.error(`${message} - ${trace}`);
+  }
+
+  warn(message: string) {
+    this.logger.warn(message);
+  }
+
+  debug(message: string) {
+    this.logger.debug(message);
+  }
+
+  verbose(message: string) {
+    this.logger.verbose(message);
+  }
+}
